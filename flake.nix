245af5eb72cb4a1909e85491f76edb6ad4879bd0
Diogo Doreto <diogo@doreto.com.br>
2025-08-10 20:08:37 +0200

    add svelte docs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,23 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    svelte = {
+      url = "github:sveltejs/svelte/svelte@5.38.0";
+      flake = false;
+    };
+    sveltekit = {
+      type = "github";
+      owner = "sveltejs";
+      repo = "kit";
+      ref = "@sveltejs/kit@2.27.3";
+      flake = false;
+    };
     flowbite-svelte = {
       url = "github:themesberg/flowbite-svelte/v1.11.4";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, utils, flowbite-svelte }: utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, utils, ... }@inputs: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -19,8 +30,15 @@
             npm install
 
             mkdir -p .private/docs
+
+            rm -rf .private/docs/svelte
+            cp -rL --no-preserve=all ${inputs.svelte}/documentation/docs .private/docs/svelte
+
+            rm -rf .private/docs/sveltekit
+            cp -rL --no-preserve=all ${inputs.sveltekit}/documentation/docs .private/docs/sveltekit
+
             rm -rf .private/docs/flowbite-svelte
-            cp -rL --no-preserve=all ${flowbite-svelte}/static/llm .private/docs/flowbite-svelte
+            cp -rL --no-preserve=all ${inputs.flowbite-svelte}/static/llm .private/docs/flowbite-svelte
           '';
         };
       }
