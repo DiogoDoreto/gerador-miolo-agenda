cc487f8bd1b643d20710cff5b5b507fda5b3a875
Diogo Doreto <diogo@doreto.com.br>
2025-08-10 12:37:55 +0200

    add flowbite-svelte docs to the project

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    flowbite-svelte = {
+      url = "github:themesberg/flowbite-svelte/v1.11.4";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, flowbite-svelte }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -13,6 +17,10 @@
           ];
           shellHook = ''
             npm install
+
+            mkdir -p .private/docs
+            rm -rf .private/docs/flowbite-svelte
+            cp -rL --no-preserve=all ${flowbite-svelte}/static/llm .private/docs/flowbite-svelte
           '';
         };
       }
